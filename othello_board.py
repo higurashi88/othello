55ed14d673e3601f26ff3a2b1b499e69e8db799d
higurashi88 <msippc1997@gmail.com>
2024-04-17 00:35:05 +0900

    add cells

--- othello_board.py
+++ othello_board.py
@@ -2,14 +2,34 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Reversi"
+
+    page.title = "Othello"
 
     # ウィンドウの上下左右から中央に配置する
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
 
-    # 背景を描画(page)
-    page.add(ft.Rectangle(width=500, height=500, fill="green"))
+    cell = ft.Container(
+        bgcolor=ft.colors.GREEN,
+        width=75,
+        height=75,
+        border_radius=10,
+        on_click=lambda e: print("Clickable without Ink clicked!"),
+    )
+
+    culumn = []
+    for i in range(8):
+        culumn.append(cell)
+
+    row = []
+    for i in range(8):
+        row.append(ft.Row(culumn))
+
+    page.add(
+        ft.Column(
+            row
+        )
+    )
 
 
 ft.app(target=main)
